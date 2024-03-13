99f8fbfb1500984fde82baa236709ec0aec29a00
Pavan M <110841565+pavanm555@users.noreply.github.com>
2024-03-13 19:17:15 +0530

    Add files via upload

--- 14_shufflle_array/ShuffleArray.java
+++ 14_shufflle_array/ShuffleArray.java
@@ -2,8 +2,14 @@ import java.util.*;
 
 public class ShuffleArray {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6,7,8,9};
+        Integer arr[] = {1,2,3,4,5,6,7,8,9};
         List<Integer> intList = Arrays.asList(arr);
+
+        Collections.shuffle(intList);
+
+        intList.toArray(arr);
+        System.out.println(Arrays.toString(arr));
+
 
     }
 }
